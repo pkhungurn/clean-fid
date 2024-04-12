94df83f4a96f7e201c81d80beb9cc99f48339f1c
pong <pong@pixiv.co.jp>
2024-04-12 12:14:03 +0900

    make resizers into classes

--- cleanfid/resize.py
+++ cleanfid/resize.py
@@ -18,14 +18,11 @@ def build_resizer(mode):
         return make_resizer("PyTorch", False, "bilinear", (299, 299))
     else:
         raise ValueError(f"Invalid mode {mode} specified")
+    
 
-
-"""
-Construct a function that resizes a numpy image based on the
-flags passed in.
-"""
-def make_resizer(library, quantize_after, filter, output_size):
-    if library == "PIL" and quantize_after:
+class PILResizer:
+    def __init__(self, output_size, filter):
+        self.output_size = output_size
         name_to_filter = {
             "bicubic": Image.BICUBIC,
             "bilinear": Image.BILINEAR,
@@ -33,12 +30,18 @@ def make_resizer(library, quantize_after, filter, output_size):
             "lanczos": Image.LANCZOS,
             "box": Image.BOX
         }
-        def func(x):
-            x = Image.fromarray(x)
-            x = x.resize(output_size, resample=name_to_filter[filter])
-            x = np.asarray(x).clip(0, 255).astype(np.uint8)
-            return x
-    elif library == "PIL" and not quantize_after:
+        self.resample = name_to_filter[filter]        
+
+    def __call__(self, x):
+        x = Image.fromarray(x)
+        x = x.resize(self.output_size, resample=self.resample)
+        x = np.asarray(x).clip(0, 255).astype(np.uint8)
+        return x
+    
+
+class PILResizerNotQuantizeAfter:
+    def __init__(self, output_size, filter):
+        self.output_size = output_size
         name_to_filter = {
             "bicubic": Image.BICUBIC,
             "bilinear": Image.BILINEAR,
@@ -46,39 +49,55 @@ def make_resizer(library, quantize_after, filter, output_size):
             "lanczos": Image.LANCZOS,
             "box": Image.BOX
         }
-        s1, s2 = output_size
-        def resize_single_channel(x_np):
-            img = Image.fromarray(x_np.astype(np.float32), mode='F')
-            img = img.resize(output_size, resample=name_to_filter[filter])
-            return np.asarray(img).clip(0, 255).reshape(s2, s1, 1)
-        def func(x):
-            x = [resize_single_channel(x[:, :, idx]) for idx in range(3)]
-            x = np.concatenate(x, axis=2).astype(np.float32)
-            return x
-    elif library == "PyTorch":
-        import warnings
-        # ignore the numpy warnings
-        warnings.filterwarnings("ignore")
-        def func(x):
-            x = torch.Tensor(x.transpose((2, 0, 1)))[None, ...]
-            x = F.interpolate(x, size=output_size, mode=filter, align_corners=False)
-            x = x[0, ...].cpu().data.numpy().transpose((1, 2, 0)).clip(0, 255)
-            if quantize_after:
-                x = x.astype(np.uint8)
-            return x
-    elif library == "TensorFlow":
-        import warnings
-        # ignore the numpy warnings
-        warnings.filterwarnings("ignore")
+        self.resample = name_to_filter[filter]
+
+    def resize_single_channel(self, x_np):
+        s1, s2 = self.output_size
+        img = Image.fromarray(x_np.astype(np.float32), mode='F')
+        img = img.resize(self.output_size, resample=self.resample)
+        return np.asarray(img).clip(0, 255).reshape(s2, s1, 1)
+    
+    def __call__(self, x):
+        x = [self.resize_single_channel(x[:, :, idx]) for idx in range(3)]
+        x = np.concatenate(x, axis=2).astype(np.float32)
+        return x
+    
+
+class PyTorchResizer:
+    def __init__(self, quantize_after, output_size, filter):        
+        self.quantize_after = quantize_after
+        self.output_size = output_size
+        self.filter = filter
+
+    def __call__(self, x):
+        x = torch.Tensor(x.transpose((2, 0, 1)))[None, ...]
+        x = F.interpolate(x, size=self.output_size, mode=self.filter, align_corners=False)
+        x = x[0, ...].cpu().data.numpy().transpose((1, 2, 0)).clip(0, 255)
+        if self.quantize_after:
+            x = x.astype(np.uint8)
+        return x
+    
+
+class TensorflowResizer:
+    def __init__(self, quantize_after, output_size, filter):        
+        self.quantize_after = quantize_after
+        self.output_size = output_size
+        self.filter = filter
+
+    def __call__(self, x):
         import tensorflow as tf
-        def func(x):
-            x = tf.constant(x)[tf.newaxis, ...]
-            x = tf.image.resize(x, output_size, method=filter)
-            x = x[0, ...].numpy().clip(0, 255)
-            if quantize_after:
-                x = x.astype(np.uint8)
-            return x
-    elif library == "OpenCV":
+        x = tf.constant(x)[tf.newaxis, ...]
+        x = tf.image.resize(x, self.output_size, method=self.filter)
+        x = x[0, ...].numpy().clip(0, 255)
+        if self.quantize_after:
+            x = x.astype(np.uint8)
+        return x
+
+
+class OpenCVResizer:
+    def __init__(self, quantize_after, output_size, filter):        
+        self.quantize_after = quantize_after
+        self.output_size = output_size
         import cv2
         name_to_filter = {
             "bilinear": cv2.INTER_LINEAR,
@@ -87,16 +106,97 @@ def make_resizer(library, quantize_after, filter, output_size):
             "nearest": cv2.INTER_NEAREST,
             "area": cv2.INTER_AREA
         }
-        def func(x):
-            x = cv2.resize(x, output_size, interpolation=name_to_filter[filter])
-            x = x.clip(0, 255)
-            if quantize_after:
-                x = x.astype(np.uint8)
-            return x
+        self.interpolation = name_to_filter[filter]
+
+    def __call__(self, x):        
+        import cv2
+        x = cv2.resize(x, self.output_size, interpolation=self.interpolation)
+        x = x.clip(0, 255)
+        if self.quantize_after:
+            x = x.astype(np.uint8)
+        return x
+
+"""
+Construct a function that resizes a numpy image based on the
+flags passed in.
+"""
+def make_resizer(library, quantize_after, filter, output_size):
+    if library == "PIL" and quantize_after:
+        return PILResizer(output_size, filter)
+        #name_to_filter = {
+        #    "bicubic": Image.BICUBIC,
+        #    "bilinear": Image.BILINEAR,
+        #    "nearest": Image.NEAREST,
+        #    "lanczos": Image.LANCZOS,
+        #    "box": Image.BOX
+        #}
+        #def func(x):
+        #    x = Image.fromarray(x)
+        #    x = x.resize(output_size, resample=name_to_filter[filter])
+        #    x = np.asarray(x).clip(0, 255).astype(np.uint8)
+        #    return x
+    elif library == "PIL" and not quantize_after:        
+        return PILResizerNotQuantizeAfter(output_size, filter)
+        #name_to_filter = {
+        #    "bicubic": Image.BICUBIC,
+        #    "bilinear": Image.BILINEAR,
+        #    "nearest": Image.NEAREST,
+        #    "lanczos": Image.LANCZOS,
+        #    "box": Image.BOX
+        #}
+        #s1, s2 = output_size
+        #def resize_single_channel(x_np):
+        #    img = Image.fromarray(x_np.astype(np.float32), mode='F')
+        #    img = img.resize(output_size, resample=name_to_filter[filter])
+        #    return np.asarray(img).clip(0, 255).reshape(s2, s1, 1)
+        #def func(x):
+        #    x = [resize_single_channel(x[:, :, idx]) for idx in range(3)]
+        #    x = np.concatenate(x, axis=2).astype(np.float32)
+        #    return x
+    elif library == "PyTorch":
+        import warnings
+        # ignore the numpy warnings
+        warnings.filterwarnings("ignore")
+        return PyTorchResizer(quantize_after, output_size, filter)
+        #def func(x):
+        #    x = torch.Tensor(x.transpose((2, 0, 1)))[None, ...]
+        #    x = F.interpolate(x, size=output_size, mode=filter, align_corners=False)
+        #    x = x[0, ...].cpu().data.numpy().transpose((1, 2, 0)).clip(0, 255)
+        #    if quantize_after:
+        #        x = x.astype(np.uint8)
+        #    return x
+    elif library == "TensorFlow":
+        import warnings
+        # ignore the numpy warnings
+        warnings.filterwarnings("ignore")
+        return TensorflowResizer(quantize_after, output_size, filter)
+        #import tensorflow as tf
+        #def func(x):
+        #    x = tf.constant(x)[tf.newaxis, ...]
+        #    x = tf.image.resize(x, output_size, method=filter)
+        #    x = x[0, ...].numpy().clip(0, 255)
+        #    if quantize_after:
+        #        x = x.astype(np.uint8)
+        #    return x
+    elif library == "OpenCV":
+        return OpenCVResizer(quantize_after, output_size, filter)
+        #import cv2
+        #name_to_filter = {
+        #    "bilinear": cv2.INTER_LINEAR,
+        #    "bicubic": cv2.INTER_CUBIC,
+        #    "lanczos": cv2.INTER_LANCZOS4,
+        #    "nearest": cv2.INTER_NEAREST,
+        #    "area": cv2.INTER_AREA
+        #}
+        #def func(x):
+        #    x = cv2.resize(x, output_size, interpolation=name_to_filter[filter])
+        #    x = x.clip(0, 255)
+        #    if quantize_after:
+        #       x = x.astype(np.uint8)
+        #    return x
     else:
         raise NotImplementedError('library [%s] is not include' % library)
-    return func
-
+    
 
 class FolderResizer(torch.utils.data.Dataset):
     def __init__(self, files, outpath, fn_resize, output_ext=".png"):
